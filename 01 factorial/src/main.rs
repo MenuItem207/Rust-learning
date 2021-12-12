2aedfe31a63158d275520d2af7c9227f95874340
MenuItem207 <70802541+MenuItem207@users.noreply.github.com>
2021-12-12 19:47:13 +0800

    Added 01 factorial

---
+++ 01 factorial/src/main.rs
@@ -0,0 +1,31 @@
+use std::io::{stdin, stdout, BufRead};
+
+fn main() {
+    print!("Enter a number: ");
+    let n: u64 = std::io::stdin()
+        .lock()
+        .lines()
+        .next()
+        .expect("stdin should be available")
+        .expect("couldn't read from stdin")
+        .trim()
+        .parse()
+        .expect("input was not an integer");
+    println!("The factorial of {} is {}", n, factorial(n));
+}
+
+// finds the factorial of a number
+fn factorial(mut n: u64) -> u64 {
+    if n == 0 {
+        return 1;
+    }
+    let mut o: u64 = n;
+    loop {
+        if n == 1 {
+            break;
+        }
+        n = n - 1;
+        o = o * n;
+    }
+    return o;
+}
